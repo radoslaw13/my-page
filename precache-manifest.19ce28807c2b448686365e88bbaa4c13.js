3f0de57f914b8d4ca06db87087843ac7ab12e66d
Radek <rad.musial@gmail.com>
2019-03-11 11:34:44 +0100

    Updates

---
+++ precache-manifest.19ce28807c2b448686365e88bbaa4c13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "020749ed43a8efbcd0f6128df338ce15",
+    "url": "/my-page/static/media/project1.020749ed.PNG"
+  },
+  {
+    "revision": "e92831d0b514bbbdc9b9b08c11097eb8",
+    "url": "/my-page/static/media/bg.e92831d0.jpg"
+  },
+  {
+    "revision": "b02e45a9cee297061e6b",
+    "url": "/my-page/static/js/runtime~main.b02e45a9.js"
+  },
+  {
+    "revision": "302ebeda245815a163a2",
+    "url": "/my-page/static/js/main.302ebeda.chunk.js"
+  },
+  {
+    "revision": "b0a626fa1331478a7fe2",
+    "url": "/my-page/static/js/2.b0a626fa.chunk.js"
+  },
+  {
+    "revision": "302ebeda245815a163a2",
+    "url": "/my-page/static/css/main.a348ceb8.chunk.css"
+  },
+  {
+    "revision": "b20d49caaac198c08e54548546fb86f6",
+    "url": "/my-page/index.html"
+  }
+];
